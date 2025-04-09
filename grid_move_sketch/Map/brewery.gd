97493f25aca0628757632749040a10bf50a9eb02
M@ <129343478+theoneandonlymattjones@users.noreply.github.com>
2025-04-09 16:04:28 -0700

    Improved Brewery
    Now has 2 components and animates in two stages for a number of clock ticks.

--- grid_move_sketch/Map/brewery.gd
+++ grid_move_sketch/Map/brewery.gd
@@ -1,13 +1,27 @@
 extends Area2D
 
 @onready var boiler_animation_player: AnimationPlayer = $Boiler/BoilerAnimationPlayer
+@onready var fermenter_animation_player: AnimationPlayer = $Fermenter/FermenterAnimationPlayer
+
+@onready var boiler_liquid: Sprite2D = $Boiler/BoilerLiquid
+@onready var fermenter_liquid: Sprite2D = $Fermenter/FermenterLiquid
+
+@onready var boiler_audio_stream_player_2d: AudioStreamPlayer2D = $Boiler/BoilerAudioStreamPlayer2D
+
+@export var liquid_color:Color = Color.WEB_PURPLE
 
 var title:= "Brewery"
 var action:= "brew"
 
-var brewing := false
+var brew_progress: int = 0
+
+func _ready() -> void:
+	var theClock = get_node("/root/World/Clock")
+	theClock.time_has_elapsed.connect(_time_elapsed)
+
 
 func get_info() -> Dictionary:
+	update_brew_progress()
 	var use_info = {}
 	use_info.title = title
 	if action:
@@ -15,12 +29,49 @@ func get_info() -> Dictionary:
 	return use_info
 
 func use() -> bool:
-	if brewing:
+	if brew_progress == 0:
+		brew_progress = 1
+		update_brew_progress()
+		fermenter_liquid.modulate = liquid_color
+		boiler_liquid.modulate = liquid_color
+		return true
+	update_brew_progress()
+	return false
+	#if brewing:
+		#action = "brew"
+		#boiler_animation_player.play("RESET")
+		#brewing = false
+	#else:
+		#action = "stop brewing"
+		#boiler_animation_player.play("boil")
+		#brewing = true
+
+func _time_elapsed():
+	if brew_progress > 0:
+		brew_progress += 1
+		update_brew_progress()
+
+func update_brew_progress():
+	if brew_progress == 0:
+		title = "Brewery: Idle"
 		action = "brew"
 		boiler_animation_player.play("RESET")
-		brewing = false
-	else:
-		action = "stop brewing"
+		fermenter_animation_player.play("RESET")
+		boiler_audio_stream_player_2d.stop()
+		return
+	if brew_progress <= 8:
+		title = "Brewery: Brewing"
+		action = ""
 		boiler_animation_player.play("boil")
-		brewing = true
-	return true
+		fermenter_animation_player.play("RESET")
+		boiler_audio_stream_player_2d.play()
+		return
+	if brew_progress <= 20:
+		title = "Brewery: Fermenting"
+		action = ""
+		boiler_animation_player.play("RESET")
+		fermenter_animation_player.play("ferment")
+		boiler_audio_stream_player_2d.stop()
+		return
+	brew_progress = 0
+	update_brew_progress()
